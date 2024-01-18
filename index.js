d537fc526f99af6ec0c69846298e7a5616ee5793
Tunerini <mwaniki.nderi@gmail.com>
2024-01-18 09:48:00 +0300

    children and adults list

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Filter method
 
+
+// 1. Even and Odd Numbers
+
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let evenNums = numbers.filter(isEven);
 let oddNums = numbers.filter(isOdd);
@@ -12,4 +15,21 @@ function isEven(element) {
 
 function isOdd(element) {
   return element % 2 !== 0;
+}
+
+
+// 2. Adults and Children list
+const ages = [12, 14, 15, 16, 18, 22, 24, 30, 42];
+const adults = ages.filter(isAdult);
+const children = ages.filter(isChild);
+
+console.log("Children: " + children);
+console.log("Adults: " + adults);
+
+function isAdult(element) {
+  return element >= 18;
+}
+
+function isChild(element) {
+  return element < 18;
 }
